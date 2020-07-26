902ac064f8623daf71db350baaef00a01975fc3a
Leonardo Setubal <setubal43@gmail.com>
2020-07-26 18:33:58 -0300

    feat: criado header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles/css/App.css';
 import Routes from './routes'
-
+import Header from './Components/header'
 function App() {
   return (
     <div>
+      <Header />
       <Routes />
     </div>
   );
---
+++ src/Components/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.scss'
+const Header = () => {
+	return (
+		<header>
+			<h1>
+				Questões Talk
+			</h1>
+			<ul>
+				<li>
+					<a>BPM</a>
+				</li>
+				<li>
+					<a>Diagrama de Classes</a>
+				</li>
+			</ul>
+
+		</header>
+	);
+};
+
+export default Header;
